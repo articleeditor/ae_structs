8b53db83e4e7950df5d6e5b38c580f130266386b
StevenUster <stevenelias@web.de>
2023-03-22 16:32:04 +0100

    Separate product into module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use mongodb::bson::{oid::ObjectId, to_document, Bson};
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
-use serde_json::Number;
 
+pub mod product;
 pub mod user;
 
 #[derive(Deserialize, Serialize)]
@@ -10,42 +9,6 @@ pub struct Response<T, S> {
     pub success: bool,
     pub data: Option<T>,
     pub error: Option<S>,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Product {
-    pub _id: Option<ObjectId>,
-    pub shopify_id: Option<String>,
-    pub title: Option<String>,
-    pub description_html: Option<String>,
-    pub price: Option<Price>,
-}
-
-impl From<Product> for Bson {
-    fn from(product: Product) -> Bson {
-        let doc = to_document(&product).unwrap();
-        Bson::Document(doc)
-    }
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct MergeProduct {
-    pub shopify_id: Option<String>,
-    pub title: Option<String>,
-    pub description_html: Option<String>,
-    pub price: Option<Price>,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub enum Currency {
-    EUR,
-    USD,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Price {
-    pub value: Number,
-    pub currency: Currency,
 }
 
 pub fn handle_error<T, S>(e: S) -> Json<Response<T, S>> {
--- src/product.rs
+++ src/product.rs
@@ -1,13 +1,39 @@
+use mongodb::bson::{oid::ObjectId, to_document, Bson};
 use serde::{Deserialize, Serialize};
-use validator::Validate;
+use serde_json::Number;
 
-#[derive(Deserialize, Serialize, Debug, Clone, Validate)]
-pub struct User {
-    #[validate(email)]
-    pub email: String,
-    pub password: String,
-    pub first_name: String,
-    pub last_name: String,
-    pub created_at: String,
-    pub updated_at: String,
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Product {
+    pub _id: Option<ObjectId>,
+    pub shopify_id: Option<String>,
+    pub title: Option<String>,
+    pub description_html: Option<String>,
+    pub price: Option<Price>,
+}
+
+impl From<Product> for Bson {
+    fn from(product: Product) -> Bson {
+        let doc = to_document(&product).unwrap();
+        Bson::Document(doc)
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct MergeProduct {
+    pub shopify_id: Option<String>,
+    pub title: Option<String>,
+    pub description_html: Option<String>,
+    pub price: Option<Price>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub enum Currency {
+    EUR,
+    USD,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Price {
+    pub value: Number,
+    pub currency: Currency,
 }
